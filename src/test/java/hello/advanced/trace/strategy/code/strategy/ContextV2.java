a89286ef44ae9baba416a930a27073ab813735d5
CHOHYUNHWA <gusghk115@gmail.com>
2023-09-16 19:36:07 +0900

    test

--- src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
+++ src/test/java/hello/advanced/trace/strategy/code/strategy/ContextV2.java
@@ -11,6 +11,7 @@ public class ContextV2 {
         long startTime = System.currentTimeMillis();
         //비즈니스 로직 실행
         strategy.call(); //위임
+
         //비즈니스 로직 종료
         long endTime = System.currentTimeMillis();
         long resultTime = endTime - startTime;
